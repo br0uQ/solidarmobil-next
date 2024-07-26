d9153d280cc3ea088e9fe0cc2ba42b81a918b742
Joshua Schmucker <joshua.schmucker@gmail.com>
2024-07-26 11:35:34 +0200

    Fix Navigation menu for mobile

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { FaBars } from "react-icons/fa";
 import NavbarItems from "./NavbarItems";
@@ -6,19 +8,33 @@ export default function Navbar() {
   return (
     <div className="navbar bg-base-100 text-base-content z-10 fixed top-0 w-full">
       <div className="navbar-start">
-        <details className="dropdown">
-          <summary className="btn btn-ghost m-1 lg:hidden">
-            <FaBars />
-          </summary>
-          <NavbarItems className="menu menu-sm dropdown-content rounded-box z-[1] mt-3 w-52 p-2 shadow" />
-        </details>
         <a className="btn btn-ghost text-xl font-heading">SolidarMobil e.V.</a>
       </div>
 
       <div className="navbar-center hidden lg:flex">
         <NavbarItems className="menu menu-horizontal px-1" />
       </div>
-      <div className="navbar-end" />
+
+      <div className="navbar-end">
+        <button
+          className="btn btn-ghost lg:hidden"
+          onClick={() => document.getElementById("modal_menu").showModal()}
+        >
+          <FaBars />
+        </button>
+      </div>
+
+      <dialog id="modal_menu" className="modal">
+        <div className="modal-box min-w-full min-h-full">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-lg btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <NavbarItems className="menu menu-lg bg-neutral text-neutral-content z-[1] w-full h-full items-center shadow pt-5" />
+        </div>
+      </dialog>
     </div>
   );
 }
