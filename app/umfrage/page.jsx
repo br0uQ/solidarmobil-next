4ef1cc19ef7c7b12bc7721b46fb6eee92c78f1be
Joshua Schmucker <joshua.schmucker@gmail.com>
2024-11-12 12:41:39 +0100

    Move from next.config.mjs redirect to page permanentRedirect

---
+++ app/umfrage/page.jsx
@@ -0,0 +1,8 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function Umfrage() {
+  permanentRedirect(
+    "https://nextcloud.cloudwolke.top/apps/forms/s/g4aJAGbbneQiXF2XDKPTCMyK",
+    "replace",
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: "/umfrage",
-        destination: "https://nextcloud.cloudwolke.top/apps/forms/s/g4aJAGbbneQiXF2XDKPTCMyK",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
